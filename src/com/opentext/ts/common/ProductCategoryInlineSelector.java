a79778f9792203975e9e15f99334bfcbba3958a6
krishna <krishnak@opentext.com>
2017-01-07 18:10:46 +0800

    corrected syntax errors

--- src/com/opentext/ts/common/ProductCategoryInlineSelector.java
+++ src/com/opentext/ts/common/ProductCategoryInlineSelector.java
@@ -11,37 +11,39 @@ import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 public class ProductCategoryInlineSelector {
-	private static final Log LOGGER = LogFactory.getLog(ProductCategoryInlineSelector.class);
-	
-	public static void main(String path[])throws Exception
-	{
-		LOGGER.debug("Entering ProductCategoryInLineSelector::execute");	
-		try{
-			
-		
-		String filename=path[0];
-		SAXReader reader=new SAXReader();
-		Document doc=reader.read(filename);
-		LOGGER.debug("DCR Path :: "+ doc);
-		StringBuilder option=new StringBuilder("<substitution>");
-		List l=doc.selectNodes("//root/product_category_details/product_category_name");
-		for(Iterator ite=l.iterator();ite.hasNext();)
-		{
-		Node n=(Node)ite.next();
-		 option.append("<option value=").append("\"").append(n.getText()).append("\"").append(" ").append("label=\"").append(n.getText()).append("\"/>").append("\n");
-        }
-		option.append("</substitution>");
-       // System.out.println(option);
-		LOGGER.info("XML Output :: "+ option);
-		
-		PrintWriter out= new PrintWriter(System.out);
-		out.print(option);
-		out.flush();
-		
-		
-		}catch(Exception ex){
-			
-			LOGGER.debug("Error Occured :: "+ ex.getMessage());
+	private static final Log LOGGER = LogFactory
+			.getLog(ProductCategoryInlineSelector.class);
+
+	public static void main(String path[]) throws Exception {
+		LOGGER.debug("Entering ProductCategoryInLineSelector::execute");
+		try {
+
+			String filename = path[0];
+			SAXReader reader = new SAXReader();
+			Document doc = reader.read(filename);
+			LOGGER.debug("DCR Path :: " + doc);
+			StringBuilder option = new StringBuilder("<substitution>");
+			List l = doc
+					.selectNodes("//root/product_category_details/product_category_name");
+			for (Iterator ite = l.iterator(); ite.hasNext();) {
+				Node n = (Node) ite.next();
+				option.append("<option value=").append("\"")
+						.append(n.getText()).append("\"").append(" ")
+						.append("label=\"").append(n.getText()).append("\"/>")
+						.append("\n");
+			}
+			option.append("</substitution>");
+			// System.out.println(option);
+			LOGGER.info("XML Output :: " + option);
+
+			PrintWriter out = new PrintWriter(System.out);
+			out.print(option);
+			out.flush();
+
+		} catch (Exception ex) {
+
+			LOGGER.debug("Error Occured :: " + ex.getMessage());
 			ex.printStackTrace();
 		}
+	}
 }
