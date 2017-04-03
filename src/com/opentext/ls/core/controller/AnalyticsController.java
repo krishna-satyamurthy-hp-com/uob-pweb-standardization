9d5eb89c5edc88fb6feec675566531364a338b6a
gsaud <gsaud@GSAUD4R45LC2.opentext.net>
2017-04-03 11:48:21 +0530

    Updating Analytics Class

--- src/com/opentext/ls/core/controller/AnalyticsController.java
+++ src/com/opentext/ls/core/controller/AnalyticsController.java
@@ -11,7 +11,7 @@ import org.dom4j.Node;
 import com.interwoven.livesite.common.web.ForwardAction;
 import com.interwoven.livesite.runtime.RequestContext;
 import com.interwoven.livesite.runtime.model.page.RuntimePage;
-//import com.opentext.ls.core.common.UOBBaseConstants;
+
 import com.opentext.ls.core.util.LSUtils;
 import com.opentext.ls.db.utils.PropertyReader;
 
@@ -29,8 +29,8 @@ public class AnalyticsController implements Serializable {
 				LOGGER.debug("Setting page analytics only for runtime pages");
 			}
 			try{
-				//context.setParameterString("analyticsDCRPath", UOBBaseConstants.ANALYTICS_DCR_REL_PATH);
-				//context.setParameterString("analyticsDCRPath", PropertyReader.getSystemPropertyValue("ANALYTICS_DCR_REL_PATH"));
+				
+				
 				PropertyReader is= new PropertyReader();
 				context.setParameterString("analyticsDCRPath", is.getSystemPropertyValue("ANALYTICS_DCR_REL_PATH"));
 				Document analyticsDoc = LSUtils.loadDCRContent(context,"analyticsDCRPath");
@@ -125,59 +125,6 @@ public class AnalyticsController implements Serializable {
 			}
 		}
 		return fa;
-	}
-	
-	//*****************For non Analytics Sites Head Section script injection***************
-	
-	public ForwardAction injectNonAnalyticsScriptOnPage(RequestContext context){
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("Inside injectNonAnalyticsScriptOnPage");
-		}
-		if(context.isRuntime()){
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Setting page for non-analytics Head section script injection (only for runtime pages)");
-			}
-			try{
-				LOGGER.debug("Analytics getScheme::: "+ context.getRequest().getScheme());
-				LOGGER.debug("Analytics getPageName::: "+ context.getPageName());
-				LOGGER.debug("Analytics getPath::: "+ context.getSite().getPath());
-			
-				String var = context.getSite().getPath();
-				var = var.substring(var.indexOf("sites/")+5);
-				
-				URI uri= null;
-				uri = new URI(
-						context.getRequest().getScheme().toString() ,
-						null, 
-						context.getSite().getHosts().get(0).getHostName().toString(), 
-						context.getRequest().getServerPort(), 
-						var+ "/" + context.getPageName()+".page" , 
-						null, 
-						null 
-						);
-				
-				LOGGER.debug("URI ::: "+ uri);
-				LOGGER.debug("Canonical Link URL ::: "+ uri.toURL().toString());
-				
-				if(uri != null){							
-					
-					String cannonicalLinkRel= "<link rel='canonical' href='" + uri.toURL().toString() + "'  />";
-					if(cannonicalLinkRel != null && !cannonicalLinkRel.isEmpty()){
-				// ends
-				String headSectionFinal= cannonicalLinkRel;
-				context.getPageScopeData().put(RuntimePage.PAGESCOPE_HEAD_INJECTION, headSectionFinal);
-				LOGGER.debug("Canonical Link in Head section injected");
-					}
-				}
-				
-			}catch(Exception analex){
-				if(LOGGER.isErrorEnabled()){
-					LOGGER.warn("Exception while injecting HeadSection script "+analex.getMessage());
-				}
-			}
-			}
-		
-	return fa;
 	}
 	
 	
